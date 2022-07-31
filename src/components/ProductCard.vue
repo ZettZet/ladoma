d0a74e2582ffce2e39b2917bcc2fea34eb72bcf2
Fedor Kuznetsov <dmesser@yandex.ru>
2022-07-31 19:30:54 +0500

    product card stylized

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,43 +1,93 @@
 <template>
   <div class="card">
-    <img class="image" alt="placeholder" :src=imageSource>
     <div class="container">
-      <h4><strong>{{ name }}</strong></h4>
-      <p>{{ description }}</p>
-      <button @click="onClick">{{ price }}₽</button>
+      <div class="container-content">
+        <img class="image" alt="placeholder" :src=image>
+        <h4 class="title">{{ title }}</h4>
+        <p class="description">{{ description }}</p>
+      </div>
+      <button class="price" @click="onClick">{{ isAddedShow ? 'Added' : `${price}₽` }}</button>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 import {Product} from "@/models/product";
+import {ref} from "vue";
+
+const isAddedShow = ref<boolean>(false)
 
 interface IProduct extends Product {
   id: number
-  name: string
+  title: string
   description: string
   price: number
-  imageSource: string
+  image: string
 }
 
 const {product} = defineProps<{ product: IProduct }>()
-const {imageSource, name, description, price, id} = product
+const {image, title, description, price, id} = product
 const emit = defineEmits<{ (e: 'add', id: number): void }>()
 const onClick = () => {
+  isAddedShow.value = true
   emit('add', id)
+  setTimeout(() => {
+    isAddedShow.value = false
+  }, 1000)
 }
 </script>
 
 <style lang="sass" scoped>
 .card
-  max-height: 300px
+  border-radius: 10px
   max-width: 300px
-
-.image
-  width: 100%
-  height: 100px
+  background: $secondary
+  margin: 5px
+  padding: 5px
+  flex-shrink: 3
 
 .container
-  padding: 2px 16px
+  display: flex
+  flex-direction: column
+  justify-content: space-between
+  align-items: flex-end
+  height: 100%
+
+.container-content
+  *
+    margin-bottom: 5px
+
+.image
+  float: left
+  width: 100%
+  height: auto
+  object-fit: cover
+  border-radius: 10px
+
+.title
+  font-size: large
+  font-weight: bold
+  margin-bottom: 10px
+
+.description
+  margin-bottom: 10px
+
+.price
+  font-size: large
+  font-weight: bold
+  padding: 5px 10px
+  border-radius: 10px
+  border: none
+  cursor: pointer
+
+  background-color: $primary
+  color: $secondary
+
+  transition: background-color, color 0.2s ease-in-out
+
+.price:hover
+  background-color: $sub-secondary
+  color: $black
+
 
 </style>
